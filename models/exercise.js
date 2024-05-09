7315cc8c944ad4ec8e3962f44806c258c7eedb22
Choiseunggwan <159886490+Choiseunggwan@users.noreply.github.com>
2024-05-09 09:37:52 +0900

    modi exercise.js

--- models/exercise.js
+++ models/exercise.js
@@ -3,34 +3,39 @@ const Sequelize = require('sequelize');
 class Exercise extends Sequelize.Model {
     static initiate(sequelize) {
         Exercise.init({
-            // CREATE TABLE `exercise` (
-            //     `exerciseId`	INT	NOT NULL,
-            //     `exercise_name`	VARCHAR(50)	NULL,
-            //     `exercise_sort`	VARCHAR(255)	NULL,
-            //     `description`	VARCHAR(255)	NULL,
-            //     `img`	VARCHAR(255)	NULL,
-            //     `video`	VARCHAR(255)	NULL,
-            //     `createAt`	DATETIME	NOT NULL	DEFAULT NOW,
-            //     `updateAt`	DATETIME	NOT NULL	DEFAULT NOW,
-            //     `deleteAt`	DATETIME	NULL
-            // );
-            exercise_name: {
-                // type, allowNull, unique ...
-                type: Sequelize.STRING(100),
+            name: {
+                type: Sequelize.STRING(50),
                 allowNull: false,
                 unique: true,
+            },
+            sort : {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+                unique: true,
+            },
+            description : {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+            },
+            img : {
+                type: Sequelize.STRING(255),
+                allowNull: true,
+            },
+            url : {
+                type: Sequelize.STRING(255),
+                allowNull: true,
             }
         }, {
             sequelize,
-            // 240405 models/user.js
+            paranoid: true,
+            underscored: true,
             charset: "utf8",
-            collate: "utf8_general_ci"
+            collate: "utf8_general_ci",
         });
     }
     static associate(db) {
-        // 교안 참곡
-
-        // 다대다 연결테이블 자동생성 240423/node_back/models/user.js-user.js post.js-hashtag.js (through) 
+        db.Exercise.belongsToMany(db.User, {  through: 'Exercise_follow' });
+        db.Exercise.belongsToMany(db.Todo_element, {  through: 'Ele-exercise' });
     }
 };
 
