ed8c2b56a3a800b8f78070ef9f8caec1782bd24e
Hae8 <alt9ir@gmail.com>
2024-05-21 18:01:25 +0900

    food 더미데이터 생성, food 라우터, 컨트롤러 개발 중

--- routes/food.js
+++ routes/food.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { uploadFood , watercount} = require('../controllers/food');
-const { Category, Food } = require('../models');
 const router = express.Router();
+const { uploadFood , watercount, getDishes, getDish } = require('../controllers/food');
+const { verifyToken } = require('../middlewares');
 
 
 // v1/food/
@@ -9,5 +9,11 @@ router.get('/', uploadFood)
 
 // v1/food/watercount
 router.get('/watercount', watercount);
+
+// v1/food/sort/:sort
+router.get('/sort/:sort', verifyToken, getDishes)
+
+// v1/food/:id
+router.get('/:id', verifyToken, getDish)
 
 module.exports = router;
