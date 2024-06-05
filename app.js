2f3522f7fd068e54b5416134c524e2f1e42baf91
dreamgktjdejr <dreamgktjdejr@gmail.com>
2024-06-05 11:10:53 +0900

    h_24_6_4_celebriyimage_add

--- app.js
+++ app.js
@@ -85,6 +85,6 @@ app.listen(app.get('port'), ()=>{
 })
 
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 5000}`);
-});
+// app.listen(process.env.PORT || 5000, () => {
+//     console.log(`Server is running on port ${process.env.PORT || 5000}`);
+// });
